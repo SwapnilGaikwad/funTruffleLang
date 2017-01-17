527ccd85ca46b8d58f59a576f14b6112162afbc6
Swapnil Gaikwad <gaikwad.swapnil@gmail.com>
2017-01-17 01:34:00 +0000

    Add Coconut root node to fix the missing calltarget issue

---
+++ coconut/src/fun/coconut/nodes/CoconutRootNode.java
@@ -0,0 +1,27 @@
+package fun.coconut.nodes;
+
+import com.oracle.truffle.api.frame.FrameDescriptor;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.RootNode;
+import com.oracle.truffle.api.source.SourceSection;
+
+import fun.coconut.Coconut;
+
+public class CoconutRootNode extends RootNode {
+
+	@Child CoconutExpressionNode expressionNode;
+
+	protected CoconutRootNode(Class<Coconut> language, SourceSection sourceSection,
+			FrameDescriptor frameDescriptor, CoconutExpressionNode expressionNode) {
+		super(language, sourceSection, frameDescriptor);
+		this.expressionNode = expressionNode;
+	}
+
+	@Override
+	public Object execute(VirtualFrame frame) {
+		System.out.println("In execute method of CoconutRootNode...");
+		return null;
+	}
+
+
+}
