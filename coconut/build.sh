61a6a3ba54af88a41d78ce22a1a58ceef6912d7d
Swapnil Gaikwad <gaikwad.swapnil@gmail.com>
2017-01-31 00:58:07 +0000

    Add auto-parser generation to maven

--- coconut/build.sh
+++ coconut/build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+usage() {
+
+    echo "./build.sh [-c]"
+    echo "    -c : Clean the old class files"
+}
 args="compile package"
 
-while getopts "c" opt; do
+while getopts "cgh" opt; do
   case $opt in
     c) echo "Adding 'clean' target to maven"
        args="clean "${args}
        ;;
+    *) usage
+       exit 1
   esac
 done
 
