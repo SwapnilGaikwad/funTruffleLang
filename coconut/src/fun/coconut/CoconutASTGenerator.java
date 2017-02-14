6a1f67c12f1c3c3eb412e776c0219d91068e7576
Swapnil Gaikwad <gaikwad.swapnil@gmail.com>
2017-02-14 10:40:16 +0000

    Add dummy function to perform binary operation in AST generator

--- coconut/src/fun/coconut/CoconutASTGenerator.java
+++ coconut/src/fun/coconut/CoconutASTGenerator.java
@@ -1,16 +1,24 @@
 package fun.coconut;
 
+import java.util.List;
+
 import com.oracle.truffle.api.frame.FrameDescriptor;
 
 import fun.coconut.nodes.CoconutDummyExpressionNode;
+import fun.coconut.nodes.CoconutExpressionNode;
 import fun.coconut.nodes.CoconutRootNode;
 
 public class CoconutASTGenerator {
 
 	FrameDescriptor frameDescriptor;
+	List<CoconutExpressionNode> instructionList;
 
 	public CoconutASTGenerator(){
 		frameDescriptor = new FrameDescriptor();
+	}
+
+	public void createBinaryInstruction(String operator, int lhs, int rhs){
+		System.out.println("Create instruction: " + lhs + " '" + operator + "' " + rhs);
 	}
 
 	public CoconutRootNode generateAST(){
