7cd1cfaf9bf34516045a3a5b358342bc365ad7b8
Swapnil Gaikwad <gaikwad.swapnil@gmail.com>
2017-02-16 12:03:41 +0000

    Add functionality to produce unsupported operation exception

--- coconut/src/fun/coconut/CoconutASTGenerator.java
+++ coconut/src/fun/coconut/CoconutASTGenerator.java
@@ -1,5 +1,6 @@
 package fun.coconut;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.oracle.truffle.api.frame.FrameDescriptor;
@@ -7,6 +8,7 @@ import com.oracle.truffle.api.frame.FrameDescriptor;
 import fun.coconut.nodes.CoconutDummyExpressionNode;
 import fun.coconut.nodes.CoconutExpressionNode;
 import fun.coconut.nodes.CoconutRootNode;
+import fun.coconut.nodes.CoconutUnimplementedOperationNode;
 
 public class CoconutASTGenerator {
 
@@ -15,14 +17,43 @@ public class CoconutASTGenerator {
 
 	public CoconutASTGenerator(){
 		frameDescriptor = new FrameDescriptor();
+		instructionList = new ArrayList<>();
 	}
 
 	public void createBinaryInstruction(String operator, int lhs, int rhs){
 		System.out.println("Create instruction: " + lhs + " '" + operator + "' " + rhs);
+		CoconutExpressionNode result = null;
+		switch (operator) {
+		case "+" :
+			result = new CoconutUnimplementedOperationNode();
+			break;
+		case "-" :
+			result = new CoconutUnimplementedOperationNode();
+			break;
+		case "/" :
+			result = new CoconutUnimplementedOperationNode();
+			break;
+		case "*" :
+			result = new CoconutUnimplementedOperationNode();
+			break;
+		case "%" :
+			result = new CoconutUnimplementedOperationNode();
+			break;
+		default :
+			result = new CoconutUnimplementedOperationNode();
+			break;
+		}
+		instructionList.add(result);
 	}
 
 	public CoconutRootNode generateAST(){
-		CoconutRootNode rootNode = new CoconutRootNode(Coconut.class, null, frameDescriptor, new CoconutDummyExpressionNode());
+		//TODO: Create block of instructions and pass to root node to execute
+		CoconutExpressionNode node = new CoconutDummyExpressionNode();
+		if( instructionList.size() > 0){
+			//Temporary node for testing
+			node = instructionList.get(0);
+		}
+		CoconutRootNode rootNode = new CoconutRootNode(Coconut.class, null, frameDescriptor, node);
 		return rootNode;
 	}
 }
