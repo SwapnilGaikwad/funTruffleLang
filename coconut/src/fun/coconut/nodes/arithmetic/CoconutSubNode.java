0fd32356fb8b2c2ccd4477cd9d2f4a1a7ef9d1f2
Swapnil Gaikwad <gaikwad.swapnil@gmail.com>
2017-02-27 23:29:50 +0000

    Add support for floating point subtraction

--- coconut/src/fun/coconut/nodes/arithmetic/CoconutSubNode.java
+++ coconut/src/fun/coconut/nodes/arithmetic/CoconutSubNode.java
@@ -5,29 +5,31 @@ import com.oracle.truffle.api.dsl.NodeChildren;
 import com.oracle.truffle.api.dsl.Specialization;
 
 import fun.coconut.nodes.CoconutExpressionNode;
-import fun.coconut.runtime.CoconutUnsupportedOperationException;
 
 @NodeChildren({@NodeChild("left"), @NodeChild("right")})
 public abstract class CoconutSubNode extends CoconutExpressionNode {
 
 	@Specialization
 	public int subInt(int left, int right){
-		System.out.println("Adding " + left + " - " + right);
+		System.out.println("Subtracting " + left + " - " + right);
 		return left - right;
 	}
 
 	@Specialization
-	public int subFloat(float left, float right){
-		throw new CoconutUnsupportedOperationException("Unsupported Operation for floats");
+	public float subFloat(float left, float right){
+		System.out.println("Subtracting " + left + " - " + right);
+		return left - right;
 	}
 
 	@Specialization
-	public int subFloat(float left, int right){
-		throw new CoconutUnsupportedOperationException("Unsupported Operation for floats");
+	public float subFloat(float left, int right){
+		System.out.println("Subtracting " + left + " - " + right);
+		return left - (float)right;
 	}
 
 	@Specialization
-	public int subFloat(int left, float right){
-		throw new CoconutUnsupportedOperationException("Unsupported Operation for floats");
+	public float subFloat(int left, float right){
+		System.out.println("Subtracting " + left + " - " + right);
+		return (float)left - right;
 	}
 }
