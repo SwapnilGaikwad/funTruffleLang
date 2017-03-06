080068d9f75872ed9bdab8d7173db2485fba3d1b
Swapnil Gaikwad <gaikwad.swapnil@gmail.com>
2017-03-06 19:11:35 +0000

    Create function object and use it to invoke function

--- coconut/src/fun/coconut/CoconutASTGenerator.java
+++ coconut/src/fun/coconut/CoconutASTGenerator.java
@@ -9,6 +9,7 @@ import com.oracle.truffle.api.frame.FrameDescriptor;
 
 import fun.coconut.nodes.CoconutBlockNode;
 import fun.coconut.nodes.CoconutExpressionNode;
+import fun.coconut.nodes.CoconutFunctionNode;
 import fun.coconut.nodes.CoconutRootNode;
 import fun.coconut.nodes.CoconutUnimplementedOperationNode;
 import fun.coconut.nodes.arithmetic.CoconutAddNodeGen;
@@ -25,10 +26,12 @@ public class CoconutASTGenerator {
 
 	FrameDescriptor frameDescriptor;
 	List<CoconutExpressionNode> instructionList;
+	List<CoconutExpressionNode> functions;
 
 	public CoconutASTGenerator(FrameDescriptor frameDescriptor){
 		frameDescriptor = this.frameDescriptor;
 		instructionList = new ArrayList<>();
+		functions = new ArrayList<CoconutExpressionNode>();
 	}
 
 	public void createBinaryInstruction(String operator, CoconutExpressionNode lhsNode, CoconutExpressionNode rhsNode){
@@ -62,8 +65,14 @@ public class CoconutASTGenerator {
 		instructionList.add(result);
 	}
 
-	public CallTarget generateAST(){
+	public void createFunctionNode(String functionName){
 		CoconutExpressionNode node = new CoconutBlockNode(instructionList);
+		CoconutFunctionNode functionNode = new CoconutFunctionNode(functionName, node);
+		functions.add(functionNode);
+	}
+
+	public CallTarget generateAST(){
+		CoconutExpressionNode node = functions.get(0);
 		CoconutRootNode rootNode = new CoconutRootNode(Coconut.class, null, frameDescriptor, node);
 		return Truffle.getRuntime().createCallTarget(rootNode);
 	}
--- coconut/src/fun/coconut/nodes/CoconutFunctionNode.java
+++ coconut/src/fun/coconut/nodes/CoconutFunctionNode.java
@@ -2,18 +2,18 @@ package fun.coconut.nodes;
 
 import com.oracle.truffle.api.frame.VirtualFrame;
 
-public abstract class CoconutFunctionNode extends CoconutExpressionNode {
+public class CoconutFunctionNode extends CoconutExpressionNode {
 
-	private CoconutExpressionNode functionBlock;
+	@Child private CoconutExpressionNode functionBlock;
 	private String name;
 
-	public void coconutfunctionnode(String name, CoconutBlockNode functionBlock) {
+	public CoconutFunctionNode(String name, CoconutExpressionNode functionBlock) {
 		this.functionBlock = functionBlock;
 		this.name = name;
 	}
 
 	@Override
-	public Object executeVoid(VirtualFrame frame){
+	public Object executeGeneric(VirtualFrame frame) {
 		System.out.println("Executing function '" + name + "'");
 		return functionBlock.executeVoid(frame);
 	}
