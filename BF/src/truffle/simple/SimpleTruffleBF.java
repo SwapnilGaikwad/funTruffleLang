f6113f2d59225a074a7872d5d49ff9582f51e73d
Swapnil Gaikwad <gaikwad.swapnil@gmail.com>
2017-08-25 21:14:26 +0100

    Add Truffle based crude version using frame slots

--- BF/src/truffle/simple/SimpleTruffleBF.java
+++ BF/src/truffle/simple/SimpleTruffleBF.java
@@ -6,10 +6,14 @@ import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.TruffleLanguage;
+import com.oracle.truffle.api.frame.FrameSlot;
+import com.oracle.truffle.api.frame.FrameSlotKind;
+import com.oracle.truffle.api.frame.FrameSlotTypeException;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.ExplodeLoop;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.RootNode;
+
 import common.SimpleBFImpl;
 import common.SimpleBFParser.Loop;
 import common.SimpleBFParser.OpCode;
@@ -18,10 +22,14 @@ import common.SimpleBFParser.Operation;
 public class SimpleTruffleBF implements SimpleBFImpl {
 
 	private CallTarget callTarget;
+	private FrameSlot cellSlot;
+	private FrameSlot cellPositionSlot;
 
 	public void prepareAST(Operation[] parseResult) {
 		BFRootNode bfRootNode = new BFRootNode(prepareNodes(parseResult));
 		callTarget = Truffle.getRuntime().createCallTarget(bfRootNode);
+		cellSlot = bfRootNode.getFrameDescriptor().addFrameSlot(0, FrameSlotKind.Object);
+		cellPositionSlot = bfRootNode.getFrameDescriptor().addFrameSlot(1, FrameSlotKind.Int);
 	}
 
 	private BFNode[] prepareNodes(Operation[] operations) {
@@ -39,88 +47,105 @@ public class SimpleTruffleBF implements SimpleBFImpl {
 	public void runAST() {
 		callTarget.call();
 	}
-}
 
-class Memory {
-	public int[] cells= new int[1024];
-	public int position = 0;
-}
-
-class BFRootNode extends RootNode {
-
-	@Children private final BFNode[] bfNodes;
-	
-	@SuppressWarnings("deprecation")
-	public BFRootNode(BFNode[] bfNodes) {
-		super(TruffleLanguage.class, null, null);
-		this.bfNodes = bfNodes;
+	class Memory {
+		public int[] cells= new int[1024];
+		public int position = 0;
 	}
 
-	@Override
-	public Object execute(VirtualFrame frame) {
-		Memory memory = new Memory();
-		for(BFNode bfNode : bfNodes) {
-			bfNode.execute(memory);
+	class BFRootNode extends RootNode {
+
+		@Children private final BFNode[] bfNodes;
+
+		@SuppressWarnings("deprecation")
+		public BFRootNode(BFNode[] bfNodes) {
+			super(TruffleLanguage.class, null, null);
+			this.bfNodes = bfNodes;
 		}
-		return null;
+
+		@Override
+		public Object execute(VirtualFrame frame) {
+			Memory memory = new Memory();
+			frame.setObject(cellSlot, memory.cells);
+			frame.setInt(cellPositionSlot, memory.position);
+
+			for(BFNode bfNode : bfNodes) {
+				bfNode.execute(frame);
+			}
+			return null;
+		}
 	}
-}
 
-class BFNode extends Node {
+	class BFNode extends Node {
 
-	private OpCode opCode;
-	@Children private final BFNode[] children;
+		private OpCode opCode;
+		@Children private final BFNode[] children;
 
-	public BFNode(OpCode opCode, BFNode[] children){
-		this.opCode = opCode;
-		this.children = children;
-	}
+		public BFNode(OpCode opCode, BFNode[] children){
+			this.opCode = opCode;
+			this.children = children;
+		}
 
-	public void execute(Memory memory) {
-		switch(opCode) {
-		case MOVE_RIGHT: memory.position++;
-			break;
+		public void execute(VirtualFrame frame) {
+			int position;
+			try {
+				position = frame.getInt(cellPositionSlot);
 
-		case MOVE_LEFT: memory.position--;
-			break;
+				int[] cells = (int[])frame.getObject(cellSlot);
 
-		case INC_MEM: memory.cells[memory.position]++;
-			break;
+				switch(opCode) {
+				case MOVE_RIGHT: position++;
+				frame.setInt(cellPositionSlot, position);
+				break;
 
-		case DEC_MEM: memory.cells[memory.position]--;
-			break;
+				case MOVE_LEFT: position--;
+				frame.setInt(cellPositionSlot, position);
+				break;
 
-		case PRINT_MEM: printValue(memory.cells[memory.position]);
-			break;
+				case INC_MEM: cells[position]++;
+				break;
 
-		case READ_MEM:	memory.cells[memory.position] = readValue();
-			break;
+				case DEC_MEM: cells[position]--;
+				break;
 
-		case LOOP_START: while(memory.cells[memory.position] > 0){
-				executeChildren(memory);
+				case PRINT_MEM: printValue(cells[position]);
+				break;
+
+				case READ_MEM:	cells[position] = readValue();
+				break;
+
+				case LOOP_START: while(cells[position] > 0){
+					executeChildren(frame);
+					position = frame.getInt(cellPositionSlot);
 				}
-			break;
+				break;
 
-		case LOOP_END:
-			break;
+				case LOOP_END:
+					break;
+				}
+			} catch (FrameSlotTypeException e) {
+				CompilerDirectives.transferToInterpreter();
+				e.printStackTrace();
+			}
 		}
-	}
 
-	@CompilerDirectives.TruffleBoundary
-	private int readValue() {
-		@SuppressWarnings("resource")
-		Scanner scanner = new Scanner(System.in);
-		return scanner.next().charAt(0);
-	}
-	@CompilerDirectives.TruffleBoundary
-	private void printValue(int value) {
-		System.out.print((char) value);
-	}
+		@CompilerDirectives.TruffleBoundary
+		private int readValue() {
+			@SuppressWarnings("resource")
+			Scanner scanner = new Scanner(System.in);
+			return scanner.next().charAt(0);
+		}
 
-	@ExplodeLoop
-	private void executeChildren(Memory memory) {
-		for(BFNode child : children) {
-			child.execute(memory);
+		@CompilerDirectives.TruffleBoundary
+		private void printValue(int value) {
+			System.out.print((char) value);
+		}
+
+		@ExplodeLoop
+		private void executeChildren(VirtualFrame frame) {
+			for(BFNode child : children) {
+				child.execute(frame);
+			}
 		}
 	}
 }
